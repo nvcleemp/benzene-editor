2aaad8d2d9b94cd9f35edbde7b198657243b734a
nvcleemp <nico.vancleemput@gmail.com>
2016-06-13 22:20:35 +0200

    Showing the neighbouring hexagons is now an option

--- src/benzene/editor/gui/BenzeneView.java
+++ src/benzene/editor/gui/BenzeneView.java
@@ -16,11 +16,17 @@ public class BenzeneView extends Pane implements InvalidationListener {
     private static final double PADDING = 1.05;
     private final Benzene benzene;
     private final Region parent;
+    private boolean showNeighbouringHexagons;
 
     public BenzeneView(Region parent, Benzene benzene) {
+        this(parent, benzene, true);
+    }
+    
+    public BenzeneView(Region parent, Benzene benzene, boolean showNeighbouringHexagons) {
         super();
         this.benzene = benzene;
         this.parent = parent;
+        this.showNeighbouringHexagons = showNeighbouringHexagons;
         benzene.addListener(this);
         parent.widthProperty().addListener(this);
         parent.heightProperty().addListener(this);
@@ -41,10 +47,12 @@ public class BenzeneView extends Pane implements InvalidationListener {
                verticalOffset = verticalCenter - Math.sqrt(0.75) * RADIUS,
                horizontalOffset = horizontalCenter - RADIUS;
         if(benzene.isEmpty()) {
-            Tile empty = new Tile(RADIUS, new Location(0, 0), benzene);
-            empty.setLayoutX(horizontalOffset);
-            empty.setLayoutY(verticalOffset);
-            getChildren().add(empty);
+            if(showNeighbouringHexagons){
+                Tile empty = new Tile(RADIUS, new Location(0, 0), benzene);
+                empty.setLayoutX(horizontalOffset);
+                empty.setLayoutY(verticalOffset);
+                getChildren().add(empty);
+            }
         } else {
             int startrow = benzene.verticalLowerBound(),
                 endrow   = benzene.verticalUpperBound(),
@@ -53,7 +61,7 @@ public class BenzeneView extends Pane implements InvalidationListener {
             for(int r = startrow - 1; r <= endrow + 1; r++) {
                 for(int c = startcol - 1; c <= endcol + 1; c++) {
                     Location location = new Location(r, c);
-                    if(benzene.isOccupied(location) || benzene.hasNeighbour(location)) {
+                    if(benzene.isOccupied(location) || (showNeighbouringHexagons && benzene.hasNeighbour(location))) {
                         Tile tile = new Tile(RADIUS, location, benzene);
                         tile.setLayoutX(horizontalOffset + (2 * c + r) * Math.sqrt(0.75) * PADDING * RADIUS);
                         tile.setLayoutY(verticalOffset + 1.5 * r * PADDING * RADIUS);
