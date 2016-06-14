ff27909d83044cb9b8c42fc980c0594afa6932c6
nvcleemp <nico.vancleemput@gmail.com>
2016-06-14 13:23:37 +0200

    Use an integer model to handle scaling

--- src/benzene/editor/gui/BenzeneView.java
+++ src/benzene/editor/gui/BenzeneView.java
@@ -8,6 +8,7 @@ import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
 import benzene.editor.Benzene;
+import benzene.editor.utils.IntegerModel;
 import benzene.editor.utils.Location;
 
 public class BenzeneView extends Pane implements InvalidationListener {
@@ -17,6 +18,7 @@ public class BenzeneView extends Pane implements InvalidationListener {
     private final Benzene benzene;
     private final Region parent;
     private boolean showNeighbouringHexagons;
+    private IntegerModel scaleModel;
 
     public BenzeneView(Benzene benzene) {
         this(null, benzene, false);
@@ -25,13 +27,23 @@ public class BenzeneView extends Pane implements InvalidationListener {
     public BenzeneView(Region parent, Benzene benzene) {
         this(parent, benzene, true);
     }
+
+    public BenzeneView(Region parent, Benzene benzene, IntegerModel scaleModel) {
+        this(parent, benzene, true, scaleModel);
+    }
     
     public BenzeneView(Region parent, Benzene benzene, boolean showNeighbouringHexagons) {
+        this(parent, benzene, showNeighbouringHexagons, new IntegerModel(0));
+    }
+    
+    public BenzeneView(Region parent, Benzene benzene, boolean showNeighbouringHexagons, IntegerModel scaleModel) {
         super();
         this.benzene = benzene;
         this.parent = parent;
         this.showNeighbouringHexagons = showNeighbouringHexagons;
+        this.scaleModel = scaleModel;
         benzene.addListener(this);
+        scaleModel.addListener(this);
         if(parent!=null){
             parent.widthProperty().addListener(this);
             parent.heightProperty().addListener(this);
@@ -48,13 +60,14 @@ public class BenzeneView extends Pane implements InvalidationListener {
 
     private void draw() {
         getChildren().clear();
+        double radius = RADIUS * Math.pow(2, scaleModel.getValue());
         double verticalCenter = parent == null ? 0 : parent.getHeight() / 2,
                horizontalCenter = parent == null ? 0 : parent.getWidth() / 2,
-               verticalOffset = verticalCenter - Math.sqrt(0.75) * RADIUS,
-               horizontalOffset = horizontalCenter - RADIUS;
+               verticalOffset = verticalCenter - Math.sqrt(0.75) * radius,
+               horizontalOffset = horizontalCenter - radius;
         if(benzene.isEmpty()) {
             if(showNeighbouringHexagons){
-                Tile empty = new Tile(RADIUS, new Location(0, 0), benzene);
+                Tile empty = new Tile(radius, new Location(0, 0), benzene);
                 empty.setLayoutX(horizontalOffset);
                 empty.setLayoutY(verticalOffset);
                 getChildren().add(empty);
@@ -68,9 +81,9 @@ public class BenzeneView extends Pane implements InvalidationListener {
                 for(int c = startcol - 1; c <= endcol + 1; c++) {
                     Location location = new Location(r, c);
                     if(benzene.isOccupied(location) || (showNeighbouringHexagons && benzene.hasNeighbour(location))) {
-                        Tile tile = new Tile(RADIUS, location, benzene);
-                        tile.setLayoutX(horizontalOffset + (2 * c + r) * Math.sqrt(0.75) * PADDING * RADIUS);
-                        tile.setLayoutY(verticalOffset + 1.5 * r * PADDING * RADIUS);
+                        Tile tile = new Tile(radius, location, benzene);
+                        tile.setLayoutX(horizontalOffset + (2 * c + r) * Math.sqrt(0.75) * PADDING * radius);
+                        tile.setLayoutY(verticalOffset + 1.5 * r * PADDING * radius);
                         getChildren().add(tile);
                     }
                 }
